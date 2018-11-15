4664e731a934dbcfd2c7498d605e29c598647e77
Kyle Durigan <kyle.durigan@gmail.com>
2018-11-15 16:58:17 -0700

    added basic HTML outline, CSS styling, pseudo-coding

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+<script
+src="https://code.jquery.com/jquery-3.3.1.min.js"
+integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+crossorigin="anonymous">
+</script>
+
+
+//Make a trivia game that asks several questions and gives you 4 options to choose. You only have 15 seconds to choose!
+    // Have a home page asking to be pressed to begin the game.
+        // .onClick event to start
+    // Display a timer countdown, question and a list of clickable answers
+        // Need to make a timer for 15 seconds
+        // Need an array of questions
+        // Need an array of answers
+    // After a question is answered, reload the screen with "Correct Answer" or "Wrong!" along with the correct answer.
+        // Start timer function
+        // Pull Question Array inject them into the document
+        // Pull Answer Options and inject them into the document
+        // When an answer is clicked 
+            // Compare to correct answer BTS
+                // Add to correctAnswer or wrongAnswer variable
+                // Clear screen, say either correct answer, wrong answer or time's up!
+                // If time runs out or wrong answer, return correct answer as well
+            // Add a timer for 10 seconds to switch to the next set of questions
+            // show gif that relates (extra credit)
+    // Without any user input, move to the next question while resetting the timer, question and answers    
+    // (extra credit) At the end, display total score along questions that were answered wrong
+
+
+// Variables
+
+
+
+//-------------------------------------------------------------------------------------------------------
+// Functions
+
+
+//-------------------------------------------------------------------------------------------------------
+// Main Processes
+
+
+
+
+//-------------------------------------------------------------------------------------------------------
